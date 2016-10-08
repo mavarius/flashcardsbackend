65ed429e78f571d389367e8131b5364b34a49f40
mavarius <metalwhirlwind@yahoo.com>
2016-10-07 17:14:28 -0700

    Installed front-end packages

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 import { render } from 'react-dom'
 
 render(
-  <h1>reactcards</h1>,
+  <h1>reactcards!</h1>,
   document.getElementById('root')
 )
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,8 @@ module.exports = {
   devtool: 'inline-source-map',
   entry: [
     'webpack-hot-middleware/client?reload=true',
+    'bootstrap-loader',
+    './src/css/style.css',
     './src/index.js'
   ],
   output: {
@@ -27,7 +29,17 @@ module.exports = {
         query: {
           presets: ['es2015', 'react']
         }
-      }
+      },
+      {
+        loader: 'url-loader?limit=10000',
+        test: /\.(png|jpg|jpeg|gif|woff)$/
+      },
+      { test: /(\.css)$/, loaders: ['style', 'css'] },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+      { test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
+      { test: /bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/, loader: 'imports?jQuery=jquery' }
     ]
   }
 }
